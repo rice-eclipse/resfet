986021b4ec9477a6ebf1dacf7c468e49946178be
Andrew Obler <andrewobler@gmail.com>
2019-09-17 15:50:32 -0500

    Started command listing

---
+++ include/commands/tcp_commands.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file tcp_commands.hpp
+ * @author Andrew Obler (andrewobler@gmail.com)
+ * @brief Controller commands sent as bytes over TCP.
+ * @version 0.1
+ * @date 2019-09-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#define SET_WATER 19
+#define UNSET_WATER 20
+#define SET_GITVC 21
+#define UNSET_GITVC 22
+
+// TODO: valve, ignition
