6d2171fbcc0193626a96cde8ba6f24f9ea243cce
Ubuntu <ubuntu@ubuntu.rice.edu>
2020-02-09 21:25:54 +0000

    Update bcm version

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-wget http://www.open.com.au/mikem/bcm2835/bcm2835-1.49.tar.gz
-tar xvfz bcm2835-1.49.tar.gz
-cd bcm2835-1.49
+wget http://www.open.com.au/mikem/bcm2835/bcm2835-1.62.tar.gz
+tar xvfz bcm2835-1.62.tar.gz
+cd bcm2835-1.62
 ./configure
 make
 sudo make install
 cd ..
-rm bcm2835-1.49.tar.gz
-rm -rf bcm2835-1.49
+rm bcm2835-1.62.tar.gz
+rm -rf bcm2835-1.62
