6bb82ee3e3582348a3bdba2622c3c7b3f7b27d19
Tommy Yuan <ty19@rice.edu>
2020-05-30 01:50:41 -0700

    Add string assert functions

--- src/libtest/libtest.cpp
+++ src/libtest/libtest.cpp
@@ -10,13 +10,13 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 #include "libtest/libtest.hpp"
 
 // TODO use the logger.
 // specify log level in command line
 // pretty printing
-// check string equals
 
 static test_stats global_stats = {0};
 static test_stats func_stats;
@@ -61,7 +61,22 @@ int test(const char *test_name, int (*test_func)(void *, TestStats), void *test_
 
 int assert_equals(int result, int expected, TestStats s, const char *assert_name) {
 	if (result == expected) {
-		printf("\t Pass: %.32s\n", assert_name);
+		printf("\t Pass: %.32s.\n", assert_name, result);
+		s->num_pass++;
+	} else {
+		printf("\t Fail: %.32s. Expected %d but result was %d\n",
+				assert_name, expected, result);
+		s->num_fail++;
+	}
+
+	s->num_total++;
+
+	return (result);
+}
+
+int assert_not_equals(int result, int expected, TestStats s, const char *assert_name) {
+	if (result != expected) {
+		printf("\t Pass: %.32s.\n", expected, result);
 		s->num_pass++;
 	} else {
 		printf("\t Fail: %.32s. Expected %d but result was %d\n",
@@ -98,4 +113,38 @@ int assert_null(void *result, TestStats s, const char *assert_name) {
 
 int assert_not_null(void *result, TestStats s, const char *assert_name) {
 	return (assert_true(result != NULL, s, assert_name));
+}
+
+int assert_string_equals(const char *result, const char *expected, int len, TestStats s, const char *assert_name) {
+	int ret; 
+
+	if ((ret = strncmp(result, expected, len)) == 0) {
+		printf("\t Pass: %.32s.\n", assert_name, result);
+		s->num_pass++;
+	} else {
+		printf("\t Fail: %.32s. Expected %s but result was %s\n",
+				assert_name, expected, result);
+		s->num_fail++;
+	}
+
+	s->num_total++;
+
+	return (ret);
+}
+
+int assert_string_not_equals(const char *result, const char *expected, int len, TestStats s, const char *assert_name) {
+	int ret;
+
+	if ((ret = strncmp(result, expected, len)) != 0) {
+		printf("\t Pass: %.32s.\n", expected, result);
+		s->num_pass++;
+	} else {
+		printf("\t Fail: %.32s. Expected %d but result was %d\n",
+				assert_name, expected, result);
+		s->num_fail++;
+	}
+
+	s->num_total++;
+
+	return (ret);
 }
